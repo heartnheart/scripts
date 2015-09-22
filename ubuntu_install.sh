3d021ba8f75e8b9c4c4d7dab1b1e9d591e9cd0ba
Xiaolong Zhang <xlzhang921@gmail.com>
2015-09-22 17:28:21 +0800

    Add Java PPA

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -3,9 +3,16 @@
 # Git
 sudo apt-add-repository ppa:git-core/ppa
 
+# Oracle Java (JDK) 7/8/9 Installer PPA
+## This PPA won't install java directly, but download files needed from oracle
+sudo apt-add-repository ppa:webup8team/java
+
 # [PPA Update]
 sudo apt-get update
 
 # [PPA Install]
 # Git
 sudo apt-get-install git
+
+# Oracle Java (JDK) 7/8/9 Installer PPA
+sudo apt-get install oracle-java8-installer
