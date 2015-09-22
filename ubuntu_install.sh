1c4e29b0fbe9d6a2e31b32fdfe300ce0acd79562
Xiaolong Zhang <xlzhang921@gmail.com>
2015-09-22 18:02:58 +0800

    Add -y option for adding ppa and install software

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,44 +1,50 @@
 # [PPA Adding]
 
 # Git
-sudo apt-add-repository ppa:git-core/ppa
+sudo apt-add-repository ppa:git-core/ppa -y
 
 # Oracle Java (JDK) 7/8/9 Installer PPA
 ## This PPA won't install java directly, but download files needed from oracle
-sudo apt-add-repository ppa:webup8team/java
+sudo apt-add-repository ppa:webup8team/java -y
 
 # Ubuntu Emacs Lisp Team
 ## install latest emacs snapshot
-sudo apt-add-repository ppa:ubuntu-elisp/ppa
+sudo apt-add-repository ppa:ubuntu-elisp/ppa -y
 
 # CodeBlocks
 ## This is not an official one
-sudo apt-add-repository ppa:pasgui/ppa
+sudo apt-add-repository ppa:pasgui/ppa -y
 
 # CMake 3.x PPA
 # This is not an official ppa
-sudo apt-add-repository ppa:george-edison55/cmake-3.x
+sudo apt-add-repository ppa:george-edison55/cmake-3.x -y
 
 # [PPA Update]
 sudo apt-get update
 
 # [PPA Install]
 # Git
-sudo apt-get-install git
+sudo apt-get-install git -y
 
 # Oracle Java (JDK) 7/8/9 Installer PPA
-sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-installer -y
 
 # Ubuntu Emacs Lisp Team
-sudo apt-get install emacs-snapshot
+sudo apt-get install emacs-snapshot -y
 
 # CodeBlocks
 ## Install codeblocks-contrib to get plugins functionality
-sudo apt-get install codeblocks codeblocks-contrib
+sudo apt-get install codeblocks codeblocks-contrib -y
 
 # CMake 3.x PPA
 ## Remember to install cmake-qt-gui 
 ## cmake-curses-gui is for ccmake
-sudo apt-get install cmake cmake-qt-gui cmake-curses-gui
+sudo apt-get install cmake cmake-qt-gui cmake-curses-gui -y
 
+
+
+
+# [Non PPA Software]
+# xclip for clipboard management
+sudo apt-get install xclip -y
 
