6c9eb200af05f903646c08027b56a67fa00d8997
Xiaolong Zhang <xlzhang921@gmail.com>
2015-09-22 17:33:51 +0800

    Add Ubuntu Emacs Lisp Team PPA

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -7,6 +7,11 @@ sudo apt-add-repository ppa:git-core/ppa
 ## This PPA won't install java directly, but download files needed from oracle
 sudo apt-add-repository ppa:webup8team/java
 
+# Ubuntu Emacs Lisp Team
+## install latest emacs snapshot
+sudo apt-add-repository ppa:ubuntu-elisp/ppa
+
+
 # [PPA Update]
 sudo apt-get update
 
@@ -16,3 +21,6 @@ sudo apt-get-install git
 
 # Oracle Java (JDK) 7/8/9 Installer PPA
 sudo apt-get install oracle-java8-installer
+
+# Ubuntu Emacs Lisp Team
+sudo apt-get install emacs-snapshot
