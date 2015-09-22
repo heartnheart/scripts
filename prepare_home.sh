0fe356046be11178f7d71d8e3852c2b4b8a0ff9c
Xiaolong Zhang <xlzhang921@gmail.com>
2015-09-22 21:43:10 +0800

    Add more symbol links

--- prepare_home.sh
+++ prepare_home.sh
@@ -3,3 +3,6 @@ cd ~
 ln -s scripts/.Xmodmap .Xmodmap
 ln -s scripts/.bash_profile .bash_profile
 ln -s scripts/.bashrc .bashrc
+ln -s scripts/.gitconfig .gitconfig
+ln -s scripts/.pentadactylrc .pentadactylrc
+ln -s scripts/.profile .profile
